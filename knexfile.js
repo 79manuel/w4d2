80d67421c54d05482df05e399cd8c2e75db70317
Manuel <prezcabellomanuel@gmail.com>
2017-06-21 03:01:27 +0000

    Added task 3 Migration files

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+const settings = require('./settings');
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      filename: './settings'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      filename: './settings'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
